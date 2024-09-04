8c764a0278be4540915ff926491a8db34ab57031
duihuhu <duihuhu@163.com>
2024-09-04 17:00:33 +0800

    debug breakdown

--- src/diffusers/pipelines/pixart_alpha/pipeline_pixart_alpha.py
+++ src/diffusers/pipelines/pixart_alpha/pipeline_pixart_alpha.py
@@ -337,7 +337,7 @@ class PixArtAlphaPipeline(DiffusionPipeline):
 
         if device is None:
             device = self._execution_device
-
+        print("prompt is not None ", prompt is not None)
         if prompt is not None and isinstance(prompt, str):
             batch_size = 1
         elif prompt is not None and isinstance(prompt, list):
@@ -789,7 +789,6 @@ class PixArtAlphaPipeline(DiffusionPipeline):
                 If `return_dict` is `True`, [`~pipelines.ImagePipelineOutput`] is returned, otherwise a `tuple` is
                 returned where the first element is a list with the generated images
         """
-        print("prompt ", prompt)
         if "mask_feature" in kwargs:
             deprecation_message = "The use of `mask_feature` is deprecated. It is no longer used in any computation and that doesn't affect the end results. It will be removed in a future version."
             deprecate("mask_feature", "1.0.0", deprecation_message, standard_warn=False)
